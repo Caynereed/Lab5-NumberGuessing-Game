4dd1f6393c2d6ab45dad9ad1b501183257901e52
Caynereed <reed146@tritons.iowacentral.edu>
2023-09-25 10:09:21 -0500

    Initial Commit

---
+++ Game1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Game1
+//
+//  Created by Cayne Reed on 9/25/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
